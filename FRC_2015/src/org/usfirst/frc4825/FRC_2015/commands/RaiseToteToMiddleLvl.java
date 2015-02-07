a4d5a04f5a66ff26cca47205f854728a54fef35b
Slava <vyacheslav.petrunyak@gmail.com>
2015-02-07 11:52:32 -0500

    fixed bugs

--- FRC_2015/src/org/usfirst/frc4825/FRC_2015/commands/RaiseToteToMiddleLvl.java
+++ FRC_2015/src/org/usfirst/frc4825/FRC_2015/commands/RaiseToteToMiddleLvl.java
@@ -24,7 +24,7 @@ public class RaiseToteToMiddleLvl extends Command {
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
-		Robot.toteLift.liftTote();
+		Robot.toteLift.raiseTote();
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
