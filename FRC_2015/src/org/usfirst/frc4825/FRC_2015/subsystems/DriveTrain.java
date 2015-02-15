c1d22629b62df1218cfacbcdf29470789cd2c3ef
Isidor Ehrlich <isidorehrlich12@gmail.com>
2015-02-15 10:29:03 -0500

    Did some stuff
    Fixed the gyro code that was bugged

--- FRC_2015/src/org/usfirst/frc4825/FRC_2015/subsystems/DriveTrain.java
+++ FRC_2015/src/org/usfirst/frc4825/FRC_2015/subsystems/DriveTrain.java
@@ -57,11 +57,7 @@ public class DriveTrain extends Subsystem {
 		int signX = (stick1.getRawAxis(4) > 1) ? 1 : -1;
 		double speed = Math.pow(stick1.getAxis(Joystick.AxisType.kY), 3);
 		double rotation = (Math.pow(stick1.getRawAxis(4), 5) * signX);
-		if (rotation < 0.5 && rotation > -0.5) {// ensures the robot goes straight within the specified dead zone using a gyro
-			Robot.driveTrain.driveStraitNoSensor(speed);
-		} else {
-			robotDrive21.arcadeDrive(speed, rotation);
-		}
+		robotDrive21.arcadeDrive(speed, rotation);
 	}
 
 	public boolean atSwitch() {
@@ -85,14 +81,148 @@ public class DriveTrain extends Subsystem {
 		angle = gyro.getAngle();
 	}
 	
+	
+	//\\Image Processing//\\
+	
+	
+	//Constants
+	NIVision.Range TOTE_HUE_RANGE = new NIVision.Range(24, 49);	//Default hue range for yellow tote
+	NIVision.Range TOTE_SAT_RANGE = new NIVision.Range(67, 255);	//Default saturation range for yellow tote
+	NIVision.Range TOTE_VAL_RANGE = new NIVision.Range(49, 255);	//Default value range for yellow tote
+	double AREA_MINIMUM = 0.5; //Default Area minimum for particle as a percentage of total image area
+	double LONG_RATIO = 2.22; //Tote long side = 26.9 / Tote height = 12.1 = 2.22
+	double SHORT_RATIO = 1.4; //Tote short side = 16.9 / Tote height = 12.1 = 1.4
+	double SCORE_MIN = 75.0;  //Minimum score to be considered a tote
+	double VIEW_ANGLE = 49.4; //View angle fo camera, set to Axis m1011 by default, 64 for m1013, 51.7 for 206, 52 for HD3000 square, 60 for HD3000 640x480
+	NIVision.ParticleFilterCriteria2 criteria[] = new NIVision.ParticleFilterCriteria2[1];
+	NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(0,0,1,1);
+	Scores scores = new Scores();
+	int toteAt = 0;
+	
+	//A structure to hold measurements of a particle
+	public class ParticleReport implements Comparator<ParticleReport>, Comparable<ParticleReport>{
+		double PercentAreaToImageArea;
+		double Area;
+		double ConvexHullArea;
+		double BoundingRectLeft;
+		double BoundingRectTop;
+		double BoundingRectRight;
+		double BoundingRectBottom;
+		
+		public int compareTo(ParticleReport r)
+		{
+			return (int)(r.Area - this.Area);
+		}
+		
+		public int compare(ParticleReport r1, ParticleReport r2)
+		{
+			return (int)(r1.Area - r2.Area);
+		}
+	};
+	
+	//Structure to represent the scores for the various tests used for target identification
+	public class Scores {
+		double Trapezoid;
+		double LongAspect;
+		double ShortAspect;
+		double AreaToConvexHullArea;
+	};
+	
+	//Uses vision processing to get to the bin
 	public void getToBin() {
 		//Will loop the image processing code until it the bin detected limit switch is pressed
 		while(!atSwitch()){
 			//I think that this should grab the current image from the webcam on the front of the robot
-			//WTF is happening here!?!?! all of the USBCamera methods return void!?!?!?!
-			//HSLImage image  = RobotMap.cameraFront.getImage();
-			//OMG!?WTF?!?  There is no constructor for this object
-			//NIVision.Image image = new NIVision.Image();
+			NIVision.Image initialImage = null;
+			NIVision.Image processedImage = null;
+			RobotMap.frontCamera.getImage(initialImage);
+			//Threshold the image looking for yellow (tote color)
+			NIVision.imaqColorThreshold(initialImage, processedImage, 255, NIVision.ColorMode.HSV, TOTE_HUE_RANGE, TOTE_SAT_RANGE, TOTE_VAL_RANGE);
+			//Display thresholded image
+			CameraServer.getInstance().setImage(processedImage);
+			//Finding the largest particle (assume that it is the tote)
+			int connectivity8 = 1;//find what this does it should work with 1
+			int numParticles = NIVision.imaqCountParticles(processedImage, connectivity8);
+			if(numParticles > 0){
+				//Measure particles and sort by particle size
+				Vector<ParticleReport> particles = new Vector<ParticleReport>();
+				for(int particleIndex = 0; particleIndex < numParticles; particleIndex++)
+				{
+					ParticleReport par = new ParticleReport();
+					par.PercentAreaToImageArea = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA);
+					par.Area = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_AREA);
+					par.ConvexHullArea = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_CONVEX_HULL_AREA);
+					par.BoundingRectTop = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_TOP);
+					par.BoundingRectLeft = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_LEFT);
+					par.BoundingRectBottom = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_BOTTOM);
+					par.BoundingRectRight = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_RIGHT);
+					particles.add(par);
+				}
+				particles.sort(null);
+				//Find the largest most bin-shaped particle
+				for(int particle = 0; particle < particles.size(); particle++){
+					scores.Trapezoid = TrapezoidScore(particles.elementAt(particle));
+					SmartDashboard.putNumber("Trapezoid", scores.Trapezoid);
+					scores.LongAspect = LongSideScore(particles.elementAt(particle));
+					SmartDashboard.putNumber("Long Aspect", scores.LongAspect);
+					scores.ShortAspect = ShortSideScore(particles.elementAt(particle));
+					SmartDashboard.putNumber("Short Aspect", scores.ShortAspect);
+					scores.AreaToConvexHullArea = ConvexHullAreaScore(particles.elementAt(particle));
+					SmartDashboard.putNumber("Convex Hull Area", scores.AreaToConvexHullArea);
+					boolean isTote = scores.Trapezoid > SCORE_MIN && (scores.LongAspect > SCORE_MIN || scores.ShortAspect > SCORE_MIN) && scores.AreaToConvexHullArea > SCORE_MIN;
+					if(isTote){
+						toteAt = particle;
+						break;
+					}
+				}
+				//Determine where the particle is in the image then turn and drive so that the particle is in the center
+				
+			} else {
+				System.err.println("No Particles Found!");//No particles found error message
+				break;
+			}
 		}
+	}
+	//
+	//Provided Code
+	//
+	/**
+	 * Converts a ratio with ideal value of 1 to a score. The resulting function is piecewise
+	 * linear going from (0,0) to (1,100) to (2,0) and is 0 for all inputs outside the range 0-2
+	 */
+	double ratioToScore(double ratio)
+	{
+		return (Math.max(0, Math.min(100*(1-Math.abs(1-ratio)), 100)));
+	}
+	/**
+	 * Method to score convex hull area. This scores how "complete" the particle is. Particles with large holes will score worse than a filled in shape
+	 */
+	double ConvexHullAreaScore(ParticleReport report)
+	{
+		return ratioToScore((report.Area/report.ConvexHullArea)*1.18);
+	}
+
+	/**
+	 * Method to score if the particle appears to be a trapezoid. Compares the convex hull (filled in) area to the area of the bounding box.
+	 * The expectation is that the convex hull area is about 95.4% of the bounding box area for an ideal tote.
+	 */
+	double TrapezoidScore(ParticleReport report)
+	{
+		return ratioToScore(report.ConvexHullArea/((report.BoundingRectRight-report.BoundingRectLeft)*(report.BoundingRectBottom-report.BoundingRectTop)*.954));
+	}
+
+	/**
+	 * Method to score if the aspect ratio of the particle appears to match the long side of a tote.
+	 */
+	double LongSideScore(ParticleReport report)
+	{
+		return ratioToScore(((report.BoundingRectRight-report.BoundingRectLeft)/(report.BoundingRectBottom-report.BoundingRectTop))/LONG_RATIO);
+	}
+
+	/**
+	 * Method to score if the aspect ratio of the particle appears to match the short side of a tote.
+	 */
+	double ShortSideScore(ParticleReport report){
+		return ratioToScore(((report.BoundingRectRight-report.BoundingRectLeft)/(report.BoundingRectBottom-report.BoundingRectTop))/SHORT_RATIO);
 	}
 }
